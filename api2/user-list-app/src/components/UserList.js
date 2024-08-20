fa8ee748290a709806397b814e916301bd710494
Ameetee <titi.raheem26@gmail.com>
2024-08-20 05:16:51 -0700

    first commit

---
+++ api2/user-list-app/src/components/UserList.js
@@ -0,0 +1,33 @@
+// src/UserList.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from JSONPlaceholder API
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setListOfUsers(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching the user data', error);
+      });
+  }, []);
+
+  return (
+    <div className="user-list">
+      <h1>User List</h1>
+      <ul>
+        {listOfUsers.map(user => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
